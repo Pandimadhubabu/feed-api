595ce65bfbe6a1699e800657495771aa4a0fcbca
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2019-04-19 23:36:48 +0530

    Update transform-feed.js

--- libraries/transform-feed.js
+++ libraries/transform-feed.js
@@ -64,14 +64,15 @@ module.exports = (feed, limit = 15) => {
                   link: item.link,
                   description: stripHTML(contentText),
                   image: image,
-                  video: image,
+                  video: null,
                   audio: null,
                   author: null,
-                  contentType:'null',
-                  content: 'null',
-                  contentBase: 'null',
-                  category: 'null',
+                  contentType:null,
+                  content: null,
+                  contentBase: null,
+                  category: null,
                   date: result.date || formatDate(item.pubDate),
+                  should_update_token: 757,
                 }
               } else {
                 return false
